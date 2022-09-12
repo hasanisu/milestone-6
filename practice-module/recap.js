1c0a0fbadb92612e98f91660336f545c9e03400d
hasanisu <hasanjimam@gmail.com>
2022-09-12 17:26:01 +0900

    recap

---
+++ practice-module/recap.js
@@ -0,0 +1,42 @@
+/*
+Must know:
+1. var, let, const (already practiced)
+2. Template string
+    2.1: create mutiline string 
+    2.2: add dynamic variable (expression) : `&{}`
+3. arrow function
+    4.1: single parameters arrow function 
+    4.2; two parameter arrow function 
+    4.3; arrow function with fucntion body.
+    and dont fokrget you have to return explicitly 
+
+4. map, filter, find
+5.optional chaining
+
+
+
+*/
+
+/*
+Good to have
+1. spread operator : [...number, 12, 45]
+2. destructuring 
+    2.1: object destructuring. how to get property name as variable.
+        const {x}= {x:3, y:5}
+    2.2: array destructuring 
+        const {a, b} = {3, 8, 12, 56}
+3. defult parameter 
+4. forEach 
+5. class, object
+6. dot notation vs bracket notation
+
+
+*/
+
+/*
+Should know the name
+1. reduce 
+2. inherritance 
+3. prototypical chain
+
+*/
