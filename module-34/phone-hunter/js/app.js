00e90a041d112c545831161007c74afc0553a974
Hasan Imam <hasanjimam@gmail.com>
2022-09-21 23:04:28 +0900

    phone hunter update

---
+++ module-34/phone-hunter/js/app.js
@@ -0,0 +1,68 @@
+const loadPhones = async(search) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhone(data.data);
+}
+
+const displayPhone = phones =>{
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.innerHTML='';
+    // display 20 phones only
+
+    // display no phones found
+    const noPhoen = document.getElementById('no-phone-massage');
+    if(phones.length === 0){
+        noPhoen.classList.remove('d-none');
+    }
+    else{
+        noPhoen.classList.add('d-none');
+    }
+    // display all phone found
+    phones = phones.slice(0,20);
+    phones.forEach( phone =>{
+        console.log(phone);
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        
+            <div class="card p-4">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.slug}</p>
+                </div>
+            </div>
+                   
+        `
+        phoneContainer.appendChild(phoneDiv);
+    })
+    // stop sppiner or loader
+    toggleSpiner(flase);
+}
+// handle search btn click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // start loader
+    toggleSpiner(true);
+    const searchField =document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value='';
+    loadPhones(searchText);
+})
+
+/* eita hosse loadding spenner, data jokhon load hobe tokhon eita dekhabe and data jokhon display hobe tokhon eita remove hoye jabe.
+eit dui jaigai kaj korbe jonno alada alada functiion na likhe ekta common function likha hoyese.
+jokhon button click hobe tokhon spenner start hobe...ei jonno button click er porei eita dewa hoyese.
+abar data jokhon display hobe tokhon loading spenner er display none hoye jabe.
+*/
+const toggleSpiner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+// loadPhones();
