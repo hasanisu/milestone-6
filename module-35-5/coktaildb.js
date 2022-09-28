93e408778ba8d6c2074c64603df94af0dbc0b035
Hasan Imam <hasanjimam@gmail.com>
2022-09-28 22:08:08 +0900

    udate task

--- module-35-5/coktaildb.js
+++ module-35-5/coktaildb.js
@@ -1,24 +1,25 @@
-const loadDrinks = () =>{
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`;
+const loadDrinks = (search) =>{
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayDrinks(data.drinks))
 }
 
 const displayDrinks = (drinks) => {
-    console.log(drinks);
+    // console.log(drinks);
     const drinksArea = document.getElementById('load-area');
-    drinks.forEach(drink =>{
-        console.log(drink);
+    drinksArea.innerHTML='';
+    drinks.forEach( drink => {
+        // console.log(drink);
         const drinkDiv = document.createElement('div');
         drinkDiv.classList.add('col');
         drinkDiv.innerHTML=`
         <div class="card">
         <img src="${drink.strDrinkThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${drink.strCategory}</h5>
+          <h5 class="card-title">${drink.strDrink}</h5>
           <p class="card-text">${drink.strInstructionsIT.slice(0, 100)}</p>
-          <button onclick="loadDetails('${drink.idDrink}')" type="button" class="btn btn-primary btn-lg">More Info</button>
+          <button onclick="loadDetails('${drink.idDrink}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#coktailDetailsModel">Veiw Details</button>
         </div>
         </div>
         `
@@ -33,6 +34,20 @@ const loadDetails = idDrink=>{
     .then(data => displayDetails(data.drinks[0]))
 }
 const displayDetails = drink =>{
-    console.log('clicked', drink);
+    console.log(drink);
+    const modalNameSet = document.getElementById('coktailDetailsModelLabel');
+    modalNameSet.innerText = drink.strDrink;
+    const BodyText = document.getElementById('body-area');
+    BodyText.innerHTML = `
+    <p>Drinks Catagory: ${drink.strDrink ? drink.strDrink: 'No drinks catagory found'}</p>
+    `
+
+
 }
-loadDrinks();
+const searchFeild = ()=>{
+    const searchArea = document.getElementById('input-something');
+    const searchText = searchArea.value;
+    console.log(searchText);
+    loadDrinks(searchText)
+} 
+loadDrinks('a');
