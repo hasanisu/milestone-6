5982615634375fa0982d3d57dd0501ee3478600e
hasanisu <hasanjimam@gmail.com>
2022-09-28 16:08:35 +0900

    dynamic details show update

--- module-35-5/api.js
+++ module-35-5/api.js
@@ -12,7 +12,7 @@ const displayData = photos =>{
         const photoDiv = document.createElement('div');
         photoDiv.classList.add('col');
         photoDiv.innerHTML=`
-        <div class="card">
+        <div onclick="loadingData(${photo.id})" class="card">
         <img src="${photo.thumbnailUrl}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${photo.title}</h5>
@@ -24,6 +24,29 @@ const displayData = photos =>{
     })
   
     
+}
+const loadingData = (id) =>{
+  // console.log('get the id', id);
+  const url = `https://jsonplaceholder.typicode.com/photos/${id}`;
+  console.log(url)
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayingData(data))
+}
+
+const displayingData = photo =>{
+  const detailsArea = document.getElementById('details-area');
+  detailsArea.innerHTML='';
+  const cretDiv = document.createElement('div');
+  cretDiv.classList.add('card');
+  cretDiv.innerHTML=`
+            <img src="${photo.url}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${photo.title}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+  `
+  detailsArea.appendChild(cretDiv);
 }
 
 loadData();
